56935a7c745c1c42b7d12bea811ca9003aba943b
Ken Wong <kenwwk960417@gmail.com>
2023-01-28 04:45:33 +0800

    changed health check msg

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router()
 
 router.get('/', function (req, res) {
-    res.send('Auth Server Health check success.');
+    res.send('Source Server Health check success.');
 })
 
 export default router
